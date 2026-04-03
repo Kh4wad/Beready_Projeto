fa98ffcfd9cbc1c9494a1d8ee2023411efce5c51
BRAIAN GASPAR DA ROSA <103344009+BraianGaspar@users.noreply.github.com>
2026-04-02 21:51:42 -0300

    Adicionado routes.

--- backend/config/paths.php
+++ backend/config/paths.php
@@ -1,116 +1,33 @@
 <?php
-/**
- * CakePHP(tm) : Rapid Development Framework (https://cakephp.org)
- * Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- *
- * Licensed under The MIT License
- * Redistributions of files must retain the above copyright notice.
- *
- * @copyright     Copyright (c) Cake Software Foundation, Inc. (https://cakefoundation.org)
- * @link          https://cakephp.org CakePHP(tm) Project
- * @since         3.0.0
- * @license       MIT License (https://opensource.org/licenses/mit-license.php)
- */
+declare(strict_types=1);
 
-/*
- * Use the DS to separate the directories in other defines
- */
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
+namespace App;
 
-/*
- * These definitions should only be edited if you have cake installed in
- * a directory layout other than the way it is distributed.
- * When using custom settings be sure to use the DS and do not add a trailing DS.
- */
+use Cake\Http\BaseApplication;
+use Cake\Http\MiddlewareQueue;
+use Cake\Routing\RouteBuilder;
+use App\Middleware\CorsMiddleware;
 
-/*
- * The full path to the directory which holds "src", WITHOUT a trailing DS.
- */
-if (!defined('ROOT')) {
-    define('ROOT', dirname(__DIR__));
-}
+class Application extends BaseApplication
+{
+    public function bootstrap(): void
+    {
+        parent::bootstrap();
+    }
 
-/*
- * The actual directory name for the application directory. Normally
- * named 'src'.
- */
-if (!defined('APP_DIR')) {
-    define('APP_DIR', 'src');
-}
+    public function middleware(MiddlewareQueue $middlewareQueue): MiddlewareQueue
+    {
+        // Adicionar CORS primeiro
+        $middlewareQueue->add(new CorsMiddleware());
 
-/*
- * Path to the application's directory.
- */
-if (!defined('APP')) {
-    define('APP', ROOT . DS . APP_DIR . DS);
-}
+        return $middlewareQueue;
+    }
 
-/*
- * Path to the config directory.
- */
-if (!defined('CONFIG')) {
-    define('CONFIG', ROOT . DS . 'config' . DS);
-}
-
-/*
- * File path to the webroot directory.
- */
-if (!defined('WWW_ROOT')) {
-    define('WWW_ROOT', ROOT . DS . 'webroot' . DS);
-}
-
-/*
- * Path to the tests directory.
- */
-if (!defined('TESTS')) {
-    define('TESTS', ROOT . DS . 'tests' . DS);
-}
-
-/*
- * Path to the temporary files directory.
- */
-if (!defined('TMP')) {
-    define('TMP', ROOT . DS . 'tmp' . DS);
-}
-
-/*
- * Path to the logs directory.
- */
-if (!defined('LOGS')) {
-    define('LOGS', ROOT . DS . 'logs' . DS);
-}
-
-/*
- * Path to the cache files directory. It can be shared between hosts in a multi-server setup.
- */
-if (!defined('CACHE')) {
-    define('CACHE', TMP . 'cache' . DS);
-}
-
-/*
- * Path to the resources directory.
- */
-if (!defined('RESOURCES')) {
-    define('RESOURCES', ROOT . DS . 'resources' . DS);
-}
-
-/*
- * The absolute path to the "cake" directory, WITHOUT a trailing DS.
- *
- * CakePHP should always be installed with composer, so look there.
- */
-if (!defined('CAKE_CORE_INCLUDE_PATH')) {
-    define('CAKE_CORE_INCLUDE_PATH', ROOT . DS . 'vendor' . DS . 'cakephp' . DS . 'cakephp');
-}
-
-/*
- * Path to the cake directory.
- */
-if (!defined('CORE_PATH')) {
-    define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-}
-if (!defined('CAKE')) {
-    define('CAKE', CORE_PATH . 'src' . DS);
+    public function routes(RouteBuilder $routes): void
+    {
+        parent::routes($routes);
+        
+        // Carrega o arquivo de rotas
+        require CONFIG . 'routes.php';
+    }
 }
